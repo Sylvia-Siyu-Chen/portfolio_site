e48eb10418342012c4d3e698e7639b468f01e5f4
Sylv <sc8043@nyu.edu>
2022-04-04 01:45:15 -0400

    fix duplicate react bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,38 +3,72 @@ import Sylvia from '../img/Sylvia.JPG'
 import CarouselControll from './Carousel'
 import signiture from '../img/signiture1.png'
 import { Container, Row, Col } from 'react-bootstrap'
+import Granim from "granim"
+import CustomCursorContext from './CursorContext.ts'
+import  React ,{useEffect, useContext} from 'react'
+// import StarfieldAnimation from 'react-starfield-animation'
 
 
-function Home() {
+const Home = () => {
+
     // const perf = require('./HomeScript.html');
+
+    try{  
+    useEffect(() => {
+        new Granim({
+          element: "#logo-canvas",
+          direction: "left-right",
+          opacity: [1, 1],
+          states: {
+            "default-state": {
+              gradients: [
+                ["#ee9ca7", "#0f9b0f"],
+                ["#ffdde1", "#ff9a9e"],
+                ["#ff7b59", "#ff4d00"],
+                ["#ff9a9e", "#ffdde1"],
+                ["#ff4d00", "#ff7b59"],
+                ["#ffdde1", "#ff9a9e"],
+              ],
+              transitionSpeed: 2000,
+            },
+          },
+        });
+      }, []);
+    }
+    catch(error){
+        console.log("error here!!!!:", error)
+    }
 
     return(
 
         <div className="intro">
-            <Container>
+
+            <canvas id='logo-canvas'></canvas>
+
+            {/* <Container>
                 <Row>
 
                     <Col>
-                        {/* <div className="intro_photo" >
+                        <div className="intro_photo" >
                             <img src={Sylvia}></img>
-                        </div> */}
+                        </div>
                     </Col>
 
-                    <Col>
+                    <Col> */}
                         <div className='intro_text' >
                             <div> hi stranger, thank you for dropping by  </div>
                                 <div id='violet'>this site is about someone named sylvia(siyu) chen, who</div> 
-                                    <div>writes <b>code</b></div>
+                                    <div>writes <span>code</span></div>
                                     <div>takes photos</div>
                                     <div>reads albert camus</div>
-                                    <div>likes typing in lowercase</div>
+                                    <div>types in lowercase</div>
                             <div>and welcomes bribes in the form of a leica lens or Kodak 35mm roll films </div>
                         </div>
                         <img className='sig' src={signiture} size={40}/>
-                    </Col>
+                    {/* </Col>
 
                 </Row>
-            </Container>
+            </Container> */}
 
             <div className='intro-project'>
                 {/* <CarouselControll/> */}
